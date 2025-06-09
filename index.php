dd5918325e24835ada5b8551725bde9dfc50f8c1
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-06-09 12:06:08 +0200

    START

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>password</title>
+</head>
+<body>
+    <h1>Password generator</h1>
+
+    <form action="password_get" method="GET">
+        Lunghezza password: <input type="number"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
