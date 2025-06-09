ac6a94a5bc4ef7f3e76ecec824ba5f59603bf6e5
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-06-09 16:25:15 +0200

    ADD style

--- index.php
+++ index.php
@@ -4,15 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>password</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Password generator</h1>
+    <div class="container">
 
-    <form action="password_get.php" method="GET">
-        Lunghezza password: <input type="number" name="password_target"> <br>
-        <input type="submit">
-    </form>
+    
+        <h1>Password generator</h1>
 
+        <form action="password_get.php" method="GET">
+            <span class="fw-bold">Lunghezza password:</span> <input type="number" name="password_target"> <br>
+            <input type="submit">
+        </form>
+    </div>
     
 </body>
 </html>
--- password_get.php
+++ password_get.php
@@ -4,8 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>password_info</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container">
+
+    <span>Your Password is :</span> <br>
+
+    <span class="fw-bold">
+
+    
     <?php
         // echo $_GET['password_target']
 
@@ -13,10 +21,13 @@
         $number= number_format($string);
 
         function generateRandomString($length) {
-            return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+        return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
             }
 
         echo  generateRandomString($number);
     ?>
+
+    </span>
+    </div>
 </body>
 </html>
