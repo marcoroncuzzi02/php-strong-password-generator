02732d67eaabc7f16a44fae956023c89483a1bd5
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-06-09 16:13:07 +0200

    ADD : logic

--- password_get.php
+++ password_get.php
@@ -7,7 +7,16 @@
 </head>
 <body>
     <?php
-        echo $_GET['password_target']
+        // echo $_GET['password_target']
+
+        $string= $_GET['password_target'];
+        $number= number_format($string);
+
+        function generateRandomString($length) {
+            return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+            }
+
+        echo  generateRandomString($number);
     ?>
 </body>
 </html>
